c11c978225924eab7f33e0ba4f73af8743ddf394
405369-GALIANO <405369@tecnicatura.frc.utn.edu.ar>
2024-09-12 12:12:13 -0300

    subo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FileItem } from '../models/file.item.model';
-import { FILE_LIST } from '../data/file.storage';
 import { ListadoComponent } from './listado/listado.component';
+import { AltaComponent } from "./alta/alta.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListadoComponent],
+  imports: [RouterOutlet, ListadoComponent, AltaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  files: FileItem[] = FILE_LIST;
   title = 'file-management';
 }
--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { FILE_LIST, OWNERS } from '../../data/file.storage';
-import { FileItem, FileType } from '../../models/file.item.model';
-import { CommonModule, DATE_PIPE_DEFAULT_OPTIONS } from "@angular/common";
+import { Component, OnChanges } from '@angular/core';
+import { FILE_LIST } from '../../data/file.storage';
+import { FileItem } from '../../models/file.item.model';
+import { CommonModule} from "@angular/common";
+import { AppComponent } from '../app.component';
+
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppComponent],
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
@@ -14,6 +16,7 @@ import { CommonModule, DATE_PIPE_DEFAULT_OPTIONS } from "@angular/common";
 
 export class ListadoComponent {
   files: FileItem[] = FILE_LIST;
+  selected_files: FileItem[] =[];
   constructor() {
     this.sortFiles();
   }
@@ -32,4 +35,30 @@ export class ListadoComponent {
     });
   }
 
+  deleteFiles(){
+    if(this.selected_files.length ==1){
+      this.files.splice(this.files.indexOf(this.selected_files[0]),1);
+      this.selected_files = [];
+    }else{
+      if(confirm("¿Está seguro de borrar " + this.selected_files.length + " archivos?")){
+        for(let i = 0; i< this.selected_files.length;i++ ){
+          this.files.splice(this.files.indexOf(this.selected_files[i]),1);
+        }
+        this.selected_files = [];
+      }
+    }
+  }
+
+  onCheckboxChange(file: FileItem, e: any) {
+    const isChecked = e.target.checked;
+    if(isChecked){
+      if(!this.selected_files.includes(file,0)){
+        this.selected_files.push(file);
+      }else{
+        if(this.selected_files.includes(file,0)){
+          this.selected_files.splice(this.selected_files.indexOf(file));
+        }
+      }
+    }
+  }
 }
